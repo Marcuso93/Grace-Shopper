578cbe408aafb01148b7de6cbc83c50382d5460c
Gabriel <gabecg@gmail.com>
2022-09-06 19:30:29 -0700

    test

---
+++ build/precache-manifest.c3c69247c249d94b1b8d6c2eb44ffa5d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da5be42ab2a688512a566413b663b424",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9b55ae2626436bb51fc0",
+    "url": "/static/css/main.9b8c1b41.chunk.css"
+  },
+  {
+    "revision": "5e0b0e50504c55111401",
+    "url": "/static/js/2.ab0155c1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ab0155c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b55ae2626436bb51fc0",
+    "url": "/static/js/main.a8a646de.chunk.js"
+  },
+  {
+    "revision": "7e23c3461cd3b1e0abd6",
+    "url": "/static/js/runtime-main.da5551cf.js"
+  }
+]);
