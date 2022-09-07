c434e2877fa595a4110216a24ed63600a6f68f7c
Gabriel <gabecg@gmail.com>
2022-09-06 21:22:36 -0700

    seed and client files

--- db/models/user.js
+++ db/models/user.js
@@ -1,17 +1,98 @@
 // grab our db client connection to use with our adapters
+/* eslint-disable no-useless-catch */
 const client = require('../client');
-const bcrypt = require('bcrypt')
+const bcrypt = require('bcrypt');
 
 async function createUser({ username, password}) {
   try {
+    const SALT_COUNT = 10;
+    const hashedPassword = await bcrypt.hash(password, SALT_COUNT);
 
+    const { rows: [user] } = await client.query(`
+      INSERT INTO users(username, password)
+      VALUES($1, $2)
+      ON CONFLICT (username) DO NOTHING
+      RETURNING *;
+      `, [username, hashedPassword]
+    );
+
+    delete user.password
+
+    return user
+  } catch (error) {
+    throw error;
   }
 }
-async function getAllUsers() {
-  /* this adapter should fetch a list of users from your db */
+
+async function getUser({ username, password }) {
+  try {
+    const user = await getUserByUsername(username);
+    const hashedPassword = user.password;
+
+    const isValid = await bcrypt.compare(password, hashedPassword)
+
+    if (isValid) {
+      delete user.password
+      return user
+    } else {
+      return null
+    }
+  } catch (error) {
+    throw error
+  }
 }
+
+
+
+async function getUserById(userId) {
+  try {
+    const { rows: [user] } = await client.query(`
+      SELECT *
+      FROM users
+      WHERE id = $1
+    `, [userId])
+
+    delete user.password
+
+    return user
+  } catch (error) {
+    throw error
+  }
+}
+
+async function getUserByUsername(username) {
+  try {
+    const { rows: [user] } = await client.query(`
+    SELECT  *
+    FROM users
+    WHERE username = $1
+    `, [username])
+
+    return user
+  } catch (error) {
+    throw error
+  }
+}
+
+async function getAllUsers() {
+  try {
+    const { rows } = await client.query(`
+      SELECT id, username
+      FROM users;
+    `);
+
+    return rows;
+  } catch (error) {
+    throw error;
+  }
+}
+
 
 module.exports = {
   // add your database adapter fns here
+  createUser,
+  getUser,
+  getUserById,
+  getUserByUsername,
   getAllUsers,
 };
--- db/seed.js
+++ db/seed.js
@@ -3,6 +3,7 @@ const {
   // declare your model imports here
   createUser,
   createInventory,
+  createReviews
   // for example, User
 } = require('.');
 
@@ -37,8 +38,15 @@ async function buildTables() {
       stock INTEGER,
       "isPublic" BOOLEAN DEFAULT false,
     );
+    CREATE TABLE cart(
+      id SERIAL PRIMARY KEY,
+      name VARCHAR(255),
+      price INTEGER,
+    );
     CREATE TABLE Reviews(
       id SERIAL PRIMARY KEY,
+      "creatorId" INTEGER REFERENCES users(id),
+      "itemId" INTEGER REFERENCES inventory(id),
       username VARCHAR(255) UNIQUE NOT NULL,
       stars INTEGER,
       description TEXT NOT NULL,
@@ -51,19 +59,56 @@ async function buildTables() {
     throw error;
   }
 }
-async function createInitialUsers() {
+async function createInitialData() {
   console.log("Starting to create users...")
   try {
     const usersToCreate = [
       { username: "albert", password: "bertie99" },
       { username: "sandra", password: "sandra123" },
       { username: "glamgal", password: "glamgal123" },
+      { username: "marcus", password: "1234BigMoney" },
     ]
     const users = await Promise.all(usersToCreate.map(createUser))
 
     const inventoryToCreate = [
-      { name: "", category: "",price: "", description: "", }
+      { 
+        name: "work desk", 
+        category: "table", 
+        price: "150", 
+        description: "beetle kill wood and epoxy", 
+        purchasedCount: "0",
+        stock: "0",
+        isPublic: "false"
+      },
+
+      { 
+        name: "cutting board", 
+        category: "cutting board", 
+        price: "65", 
+        description: "hard wood with custom laser engraving", 
+        purchasedCount: "0",
+        stock: "0",
+        isPublic: "false"
+      },
     ]
+    const inventory = await Promise.all(inventoryToCreate.map(createInventory))
+    
+    const cartToCreate = [
+      { name: 'albert', price: "150"},
+      { name: 'sandra', price: "65"},
+      { name: 'glamgal', price: "65"},
+      { name: 'Marcus', price: "150"},
+      { name: 'albert', price: "65"}
+    ]
+    const cart = await Promise.all(cartToCreate.map(createCart))
+
+    const reviewsToCreate =[
+      {username: "albert", creatorId: 1, itemId: 1, stars: 5, description: "This is quite possiably the best work desk thats ever existed in the history of man" },
+      {username: "sandra", creatorId: 2, itemId: 1, stars: 4, description: "This work desk is so good i can't physically leave it four stars for being waaaaay too good" },
+      {username: "glamgal", creatorId: 3, itemId: 2, stars: 4, description: "I will uses this when I cook! also this thing is so sturdy i could rely on it to fight off the law " },
+      {username: "Marcus", creatorId: 4, itemId: 2, stars: 3, description: "Could use some more wood-work." },
+    ]
+    const reviews = await Promise.all(reviewsToCreate.map(createReviews))
 
     console.log("Users created:")
     console.log(users)
@@ -74,17 +119,8 @@ async function createInitialUsers() {
   }
 }
 
-async function populateInitialData() {
-  try {
-    // create useful starting data by leveraging your
-    // Model.method() adapters to seed your db, for example:
-    // const user1 = await User.createUser({ ...user info goes here... })
-  } catch (error) {
-    throw error;
-  }
-}
 
 buildTables()
-  .then(populateInitialData)
+  .then(createInitialData)
   .catch(console.error)
   .finally(() => client.end());
