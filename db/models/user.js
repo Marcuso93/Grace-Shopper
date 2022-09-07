cbc1b17f23fc2d4667b51612972351beadd617d9
Gabriel <gabecg@gmail.com>
2022-09-06 20:31:36 -0700

    seed setup

--- db/models/user.js
+++ db/models/user.js
@@ -1,11 +1,17 @@
 // grab our db client connection to use with our adapters
 const client = require('../client');
+const bcrypt = require('bcrypt')
+
+async function createUser({ username, password}) {
+  try {
+
+  }
+}
+async function getAllUsers() {
+  /* this adapter should fetch a list of users from your db */
+}
 
 module.exports = {
   // add your database adapter fns here
   getAllUsers,
 };
-
-async function getAllUsers() {
-  /* this adapter should fetch a list of users from your db */
-}
--- db/seed.js
+++ db/seed.js
@@ -1,18 +1,76 @@
 const {
   client,
   // declare your model imports here
+  createUser,
+  createInventory,
   // for example, User
-} = require('./');
+} = require('.');
 
 async function buildTables() {
   try {
     client.connect();
+    console.log("Dropping All Tables...")
 
     // drop tables in correct order
+    await client.query(`
+      DROP TABLE IF EXISTS users;
+      DROP TABLE IF EXISTS inventory;
+      DROP TABLE IF EXISTS cart;
+      DROP TABLE IF EXISTS reviews;
+    `)
 
     // build tables in correct order
+
+    await client.query(`
+    CREATE TABLE users (
+      id SERIAL PRIMARY KEY,
+      username VARCHAR(255) UNIQUE NOT NULL,
+      password VARCHAR(255) NOT NULL
+    );
+    CREATE TABLE inventory(
+      id SERIAL PRIMARY KEY,
+      name VARCHAR(255),
+      category VARCHAR(255),
+      description TEXT NOT NULL,
+      price INTEGER,
+      purchasedCount INTEGER,
+      stock INTEGER,
+      "isPublic" BOOLEAN DEFAULT false,
+    );
+    CREATE TABLE Reviews(
+      id SERIAL PRIMARY KEY,
+      username VARCHAR(255) UNIQUE NOT NULL,
+      stars INTEGER,
+      description TEXT NOT NULL,
+    );
+    
+  `)
+    console.log("Finished building tables!");
   } catch (error) {
+    console.error("Error building tables!");
     throw error;
+  }
+}
+async function createInitialUsers() {
+  console.log("Starting to create users...")
+  try {
+    const usersToCreate = [
+      { username: "albert", password: "bertie99" },
+      { username: "sandra", password: "sandra123" },
+      { username: "glamgal", password: "glamgal123" },
+    ]
+    const users = await Promise.all(usersToCreate.map(createUser))
+
+    const inventoryToCreate = [
+      { name: "", category: "",price: "", description: "", }
+    ]
+
+    console.log("Users created:")
+    console.log(users)
+    console.log("Finished creating users!")
+  } catch (error) {
+    console.error("Error creating users!")
+    throw error
   }
 }
 
