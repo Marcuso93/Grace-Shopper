c7b903857a696a2c84dee53ec4d320414120c38f
Kevin Moore <kevsm5041@gmail.com>
2022-09-17 13:39:21 -0600

    routes update/ css

--- api/inventory.js
+++ api/inventory.js
@@ -3,49 +3,49 @@ const inventoryRouter = express.Router();
 const { requireAdmin } = require('./utils');
 
 const {
-    createInventory,
-    getInventory,
-    getInventoryById, 
-    //getInventoryByName,
-    deactivateInventory,
-    updateInventory,
-    getReviewsByItemId,
+  createInventory,
+  getInventory,
+  getInventoryById,
+  //getInventoryByName,
+  deactivateInventory,
+  updateInventory,
+  getReviewsByItemId,
 } = require('../db')
 
 // TODO: create a way to attach reviews to inventory item
 // TODO: test requireAdmin
 
 //api requests below
-inventoryRouter.get('/', async(req, res, next) => {
+inventoryRouter.get('/', async (req, res, next) => {
   try {
     const getAllInventory = await getInventory();
 
     res.send(getAllInventory);
-  } catch ({name, message}){
-      next({name, message});
+  } catch ({ name, message }) {
+    next({ name, message });
   }
 });
 
-inventoryRouter.get('/:inventoryId', async(req, res, next) => {
-  const {inventoryId} = req.params;
+inventoryRouter.get('/:inventoryId', async (req, res, next) => {
+  const { inventoryId } = req.params;
 
   try {
     const item = await getInventoryById(inventoryId);
-    
+
     res.send(item);
-  } catch ({name, message}) {
-    next({name, message})
+  } catch ({ name, message }) {
+    next({ name, message })
   }
 })
 
-inventoryRouter.get('/:inventoryId/reviews', async(req, res, next) => {
-  const {inventoryId} = req.params;
+inventoryRouter.get('/:inventoryId/reviews', async (req, res, next) => {
+  const { inventoryId } = req.params;
   try {
     const reviews = await getReviewsByItemId(inventoryId);
 
     res.send(reviews);
-  } catch({name, message}){
-     next({name, message});
+  } catch ({ name, message }) {
+    next({ name, message });
   }
 })
 
@@ -54,7 +54,7 @@ inventoryRouter.get('/:inventoryId/reviews', async(req, res, next) => {
 //   try {
 //     const { inventoryId } = req.params;
 //     const getInventoryId = await getInventoryById(inventoryId);
-    
+
 //     res.send(getInventoryId)
 //   } catch({name, message}){
 //     next({name, message})
@@ -64,13 +64,13 @@ inventoryRouter.get('/:inventoryId/reviews', async(req, res, next) => {
 inventoryRouter.post('/', requireAdmin, async (req, res, next) => {
   const { name, description, price, purchasedCount, stock, isPublic, isCustomizable } = req.body;
   const inventoryObj = { name, description, price, purchasedCount, stock, isPublic, isCustomizable };
-  
+
   try {
     const newInventory = await createInventory(inventoryObj);
 
     res.send(newInventory);
-  } catch ({name, message}){
-    next({name, message});
+  } catch ({ name, message }) {
+    next({ name, message });
   }
 })
 
@@ -79,23 +79,23 @@ inventoryRouter.patch('/:inventoryId', requireAdmin, async (req, res, next) => {
   const { ...fields } = req.body;
 
   try {
-    const updatedInventoryItem = await updateInventory({inventoryId, ...fields});
-    
+    const updatedInventoryItem = await updateInventory({ inventoryId, ...fields });
+
     res.send(updatedInventoryItem);
-  } catch ({name, message}){
-    next({name, message});
+  } catch ({ name, message }) {
+    next({ name, message });
   }
 })
 
 inventoryRouter.delete('/:inventoryId', requireAdmin, async (req, res, next) => {
   const { inventoryId } = req.params;
-  
+
   try {
     const deactivatedItem = await deactivateInventory({ inventoryId });
 
     res.send(deactivatedItem);
-  } catch ({name, message}){
-    next({name, message});
+  } catch ({ name, message }) {
+    next({ name, message });
   }
 })
 
