24d1af726c40a858e396bc5a5cd3fec2064599f9
Joe Garrett <joegarrett404@gmail.com>
2022-10-04 22:04:36 -0600

    attempting to deploy

---
+++ build/precache-manifest.375169150bb361251f477c611708cb84.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "228acda18567d2ecdf7f88d280fe6585",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0b761b9ce5b12216286b",
+    "url": "/static/css/main.51fcda68.chunk.css"
+  },
+  {
+    "revision": "4b0869a7f93365eb6d71",
+    "url": "/static/js/2.0d5893cb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.0d5893cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b761b9ce5b12216286b",
+    "url": "/static/js/main.bf6a5c16.chunk.js"
+  },
+  {
+    "revision": "7e23c3461cd3b1e0abd6",
+    "url": "/static/js/runtime-main.da5551cf.js"
+  },
+  {
+    "revision": "5a79c78ee04380cbb7cbbb5baf91ca81",
+    "url": "/static/media/WoodBackground.5a79c78e.jpg"
+  }
+]);
